00c12ddc438d214fe388a31910d5b28562be0370
Jeffrey Davis <davisj0214@yahoo.com>
2019-07-12 16:44:18 -0500

    project completed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,115 +1,214 @@
-// Set up the click functions
-// Set up the variables
-// Set up the arrays for the questions, options, and answers
-// Create a function to start the game
-// Create a function to go to the next question
-// Create a function to check the answers 
-// Create a function to end the game
-// Create a function to reset the stats and questions for the game
-// Create a timer for the game
+// =============== TRIVIA ARRAY HOLDING QUESTIONS, OPTIONS, AND ANSWERS INDEX ===============
+var triviaQuestions = [
+    {
+        question: "At what age does Harry first attend Hogwarts?",
+        options: ["8", "9", "11", "13"],
+        answer: "11"
+    },
+    {
+        question: "What class is taught by a different professor every year?",
+        options: ["Charms", "Potions", "Herbology", "Defense Against the Dark Arts"],
+        answer: "Defense Against the Dark Arts"
+    },
+    {
+        question: "Which ingredient for Polyjuice Potion needs to be picked at the full moon to be effective?",
+        options: ["Knotgrass", "Lacewing Flies", "Fluxweed", "Horn of Bicorn"],
+        answer: "Fluxweed"
+    },
+    {
+        question: "Who dies at the end of the Tri-Wizard Tournament?",
+        options: ["Viktor Krum", "Fleur DeLacour", "Cedric Diggory", "Dobby"],
+        answer: "Cedric Diggory"
+    },
+    {
+        question: "Who kills Professor Dumbledore?",
+        options: ["Draco Malfoy", "Severus Snape", "Voldemort", "Harry Potter"],
+        answer: "Severus Snape"
+    },
+    {
+        question: "What does the Marauder's Map show?",
+        options: ["Hidden treasure", "The location of everyone at Hogwarts", "The Room of Requirement", "The Weasley's secret stash"],
+        answer: "The location of everyone at Hogwarts"
+    },
+    {
+        question: "What position does Harry play on his Quidditch team?",
+        options: ["Bludger", "Chaser", "Seeker", "Keeper"],
+        answer: "Seeker"
+    },
+    {
+        question: "What house at Hogwarts does Draco Malfoy belong to?",
+        options: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
+        answer: "Slytherin"
+    },
+    {
+        question: "According to Mr. Ollivander, James Potter's wand was exceptionally good for what?",
+        options: ["Transfiguration", "Charms", "Wand Work", "Nothing"],
+        answer: "Transfiguration"
+    },
+    {
+        question: "What is the name of Harry's owl?",
+        options: ["Hedwig", "Scabbers", "Buckbeak", "Hermoine"],
+        answer: "Hedwig"
+    },
+];
 
-// .ready function for when the page loads
+// =============== VARIABLES FOR THE GAME ===============
+var userGuess;
+var correctGuess;
+var triviaIndex;
+var currentQuestion;
+var newQuestion;
+var answerOptionButton;
+var intervalId;
+var timer = 20;
+var correct = 0;
+var incorrect = 0;
+var questionCount = 0;
+var gameEnd = false;
+
+// =============== READY FUNCTION ON PAGE LOAD ===============
 $(document).ready(function() {
 
-    // Variables for the game
-    var correct = 0;
-    var incorrect = 0;
-    var unanswered = 0;
-    var timer = 45;
-    var triviaQuestion = [];
+    $("#seconds-timer").hide();
+    $("#container-trivia").hide();
+    $("#container-answers").hide();
 
-    // Function to start the game
-    function gameStart() {
+    // CLICK EVENT FOR THE START BUTTON
+    $("#start-button").click(function() {
 
-        // Arrays for Question, options, and the answer
-        triviaQuestion = [{
-            question: "At what age does Harry first attend Hogwarts?",
-            options: ["8", "9", "11", "13"],
-            answer: "11"
-        },
-                    {
-            question: "What is the name of Harry's owl?",
-            options: ["Hedwig", "Scabbers", "Buckbeak", "Hermione"],
-            answer: "Hedwig"
-        },          
-                    {
-            question: "What class is taught by a different professor every year?",
-            options: ["Charms", "Potions", "Herbology", "Defense Against the Dark Arts"],
-            answer: "Defense Against the Dark Arts"
-        },
-                    {
-            question: "What house at Hogwarts does Draco belong to?",
-            options: ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"],
-            answer: "Slytherin"
-        },
-                    {
-            question: "What position does Harry play on his Quidditch team?",
-            options: ["Bludger", "Chaser", "Seeker", "Keeper"],
-            answer: "Seeker"
-        },          
-                    {
-            question: "Who is Fluffy?",
-            options: ["Hagrid's Dragon", "Ron's Rat", "Hermione's Cat", "A Three-Headed Dog"],
-            answer: "A Three-Headed Dog"
-        },          
-                    {
-            question: "Who kills Professor Dumbledore?",
-            options: ["Draco Malfoy", "Severus Snape", "Voldemort", "Harry Potter"],
-            answer: "Severus Snape"
-        },
-                    {
-            question: "What does the Marauder's Map show?",
-            options: ["Hidden Treasure", "The Location of Everyone at Hogwarts", "The Room of Requirement", "The Weasley's Secret Stash"],
-            answer: "The Location of Everyone at Hogwarts"
-        },          
-                    {
-            question: "What is Harry's Patronus?",
-            options: ["An Owl", "A Bunny", "A Unicorn", "A Stag"],
-            answer: "A Stag"
-        },      
-                    {
-            question: "Who dies at the end of the Tri-Wizard Tournament?",
-            options: ["Viktor Krum", "Fleur DeLacour", "Cedric Diggory", "Dobby"],
-            answer: "Cedric Diggory"
-        },          
-                    {
-            question: "How many Horcruxes must Harry destroy to kill Voldemort?",
-            options: ["20", "4", "7", "9"],
-            answer: "7"
-        }, 
-                    {
-            question: "What is Dumbledore buried with?",
-            options: ["The Resurrection Stone", "The Elder Wand", "Harry's Firebolt", "The Sword of Gryffindor"],
-            answer: "The Elder Wand"
-        }];
+        $("#start").hide();
+        $("#container-instructions").hide();
+        $("#seconds-timer").show();
+        $("#container-trivia").show();
 
-        $("#answers").hide();
+        timerRun();
+        generateQuestions();
+    });
+
+    // FUNCTION FOR THE TIMER COUNT DOWN
+    function timerRun() {
+        intervalId = setInterval(function() {
+            $("#timer").text(timer);
+            timer--;
+            if (timer === -1) {
+                clearInterval(intervalId);
+                gameComplete();
+            }
+        }, 1000);
     };
 
-    var secondsTimer = 45;
-    var intervalId;
+    // FUNCTION TO TRANSITION TO THE NEXT QUESTION AFTER 2 SECONDS
+    function transition() {
+        setTimeout(nextQuestion, 2000);
+    };
 
-    $("#seconds-timer").text(secondsTimer);
+    // FUNCTION TO GENERATE THE QUESTIONS AND ANSWER OPTIONS
+    function generateQuestions() {
+        $(".trivia").empty();
+        questionCount++;
 
-    gameStart();
+        if (questionCount < 11) {
+            // Generates the questions and inserts them into the HTML
+            triviaIndex = Math.floor(Math.random() * triviaQuestions.length);
+            currentQuestion = triviaQuestions[triviaIndex];
+            $("#random-questions").text(currentQuestion.question);
 
-    intervalId = setInterval(timerStart, 1000);
+            // Generates the answer options and inserts them into the HTML
+            for (var i = 0; i < currentQuestion.options.length; i++) {
+                answerOptionButton = $("<input/>").attr({type: "button", class: "answerOptionsButton", id: currentQuestion.options[i].toString(), value: currentQuestion.options[i].toString()});
 
-    // Function for the timer countdown
-    function timerStart() {
-        if (secondsTimer === 0) {
-            gameOver();
-            return console.log("Time's Up");
+                $("#trivia-answers").append(answerOptionButton).show();
+            }
+
+            // Gets the correct answer for each question
+            correctGuess = currentQuestion.answer;
+            triviaQuestions.splice(triviaIndex, 1);
+
+            // Click event for the answer options
+            $(".answerOptionsButton").one("click", function() {
+                userGuess = $(this)[0].id;
+
+                // If the user guesses correctly then...
+                if (userGuess === correctGuess) {
+                    correct++;
+                    $(this).css({
+                        color: "#000",
+                        background: "#eeba30",
+                        border: "none"
+                    });
+                    $("#random-questions").empty();
+                    $("#random-questions").text("Correct!!");
+                    clearInterval(intervalId);
+                    transition();
+                } 
+                // If the user guesses incorrectly then... 
+                else if (userGuess !== correctGuess) {
+                    incorrect++;
+                    $(this).css({
+                        color: "#fff",
+                        background: "#2a623d",
+                        border: "none"
+                    });
+                    $("#random-questions").empty();
+                    $("#random-questions").text("Wrong!  The correct answer is .. " + correctGuess + "!");
+                    clearInterval(intervalId);
+                    transition();
+                } 
+            });
         } 
-        secondsTimer--;
-        $("seconds-timer").text(secondsTimer);
+        // If all questions are answered or time runs out then gameComplete runs
+        else {
+            gameEnd = true;
+            clearInterval(intervalId);
+            timer = 0;
+            gameComplete();
+        }
     };
 
-    // Function for the end of the game
-    function gameOver() {
-
+    // FUNCTION TO CYCLE THROUGH THE QUESTIONS
+    function nextQuestion() {
         clearInterval(intervalId);
-        for ()
-        
-    }
-})
+        timerRun();
+        generateQuestions();
+    };
+
+    // FUNCTION FOR THE END OF GAME SCREEN
+    function gameComplete() {
+        clearInterval(intervalId);
+        $("#seconds-timer").hide();
+        $("#container-trivia").hide();
+        $("#container-answers").show();
+        $("#correct-answers").text(correct);
+        $("#wrong-answers").text(incorrect);
+
+        // Gives a different display based on the number of correct answers the user got
+        if (correct === 10) {
+            $(".finished-text").text("AMAZING!! You got 100%!")
+        } else if (correct >= 6) {
+            $(".finished-text").text("Good Job!! You know you're stuff!");
+        } else if (correct === 5) {
+            $(".finished-text").text("Average! You should probably watch the movies again!");
+        } else if (correct <= 4) {
+            $(".finished-text").text("That's bad! You're definitely a muggle!");
+        } 
+    };
+
+    restart();
+
+    // FUNCTION TO RESTART THE GAME ON BUTTON CLICK
+    function restart() {
+        $("#restart-game").click("click", function() {
+            $("#container-instructions").hide();
+            $("#container-answers").hide();
+            $("#seconds-timer").show();
+            $("#container-trivia").show();
+
+            correct = 0;
+            incorrect = 0;
+            timer = 20;
+
+            timerRun();
+            generateQuestions();
+        })
+    };
+});
